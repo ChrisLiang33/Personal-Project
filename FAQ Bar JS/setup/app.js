e1fb6cd33d8561697bf429c1b84298d812f29907
liangxianmai <liangxianmai@yahoo.com>
2023-08-13 14:17:38 -0400

    quick update

--- FAQ Bar JS/setup/app.js
+++ FAQ Bar JS/setup/app.js
@@ -4,15 +4,20 @@ const questions = document.querySelectorAll(".question");
 questions.forEach((question) => {
   const btn = question.querySelector(".question-btn");
   btn.addEventListener("click", () => {
+    questions.forEach((q) => {
+      if (q !== question) {
+        q.classList.remove("show-text");
+      }
+    });
     question.classList.toggle("show-text");
   });
 });
 
 // // traversing the dom
-const btns = document.querySelectorAll(".question-btn");
-btns.forEach((btn) => {
-  btn.addEventListener("click", (i) => {
-    const question = i.currentTarget.parentElement.parentElement;
-    question.classList.toggle("show-text");
-  });
-});
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (i) => {
+//     const question = i.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
