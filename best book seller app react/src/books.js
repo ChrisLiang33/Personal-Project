8c074a2757a1abf628dfc3af51decd6793cc701f
liangxianmai <liangxianmai@yahoo.com>
2023-08-25 18:39:40 -0400

    react

---
+++ best book seller app react/src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+
+export const books = [
+  {
+    title: "The Four Agreements: A Practical Guide to Personal Freedom",
+    author: "Don Miguel Ruiz",
+    img: img1,
+    id: 1,
+  },
+  {
+    title: "iron flame",
+    author: "james clear",
+    img: img2,
+    id: 2,
+  },
+  {
+    title: "fires in the dark",
+    author: "kay redfield jamison",
+    img: img3,
+    id: 3,
+  },
+];
